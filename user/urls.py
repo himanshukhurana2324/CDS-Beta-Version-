bdffab0a9cc723d040d86c0570f82d3bc887aba7
Himanshu Khurana <himanshukhurana939@gmail.com>
2024-04-23 14:55:42 +0530

    modified:   db.sqlite3 	modified:   user/__pycache__/urls.cpython-312.pyc 	modified:   user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from user import views
+from user import views as user_views
+from home import views as home_views
 
 urlpatterns = [
-    path('', views.login, name='home'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('signin', views.signedin, name='signin'),
+    path('', home_views.dashboard, name='home'),
+    path('login', user_views.login, name='login'),
+    path('register', user_views.register, name='register'),
+    path('signin', user_views.signedin, name='signin'),
   
 
 ]

7e98063f727f19521bff93f8cd6f3d6b64d3f318
Himanshu Khurana <himanshukhurana939@gmail.com>
2024-04-23 22:37:10 +0530

    modified:   db.sqlite3 	modified:   home/__pycache__/views.cpython-312.pyc 	modified:   home/views.py 	new file:   staticfiles/settings.5f30d14d6552.css 	new file:   staticfiles/settings.5f30d14d6552.css.gz 	modified:   staticfiles/settings.css 	modified:   staticfiles/settings.css.gz 	modified:   staticfiles/staticfiles.json 	modified:   templates/dashboard.html 	modified:   templates/overviewContainer.html 	modified:   user/__pycache__/urls.cpython-312.pyc 	modified:   user/urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,14 +1,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from user import views as user_views
 from home import views as home_views
 
 urlpatterns = [
-    path('', home_views.dashboard, name='home'),
+    
+    path('', RedirectView.as_view(url='home/', permanent=True), name='home'),
     path('login', user_views.login, name='login'),
     path('register', user_views.register, name='register'),
     path('signin', user_views.signedin, name='signin'),
-  
-
+    
 ]
